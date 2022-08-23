a4e1665109c450154b7a55bfcecee778c6193224
juliogayosso <j_chalo@hotmail.com>
2022-08-23 18:07:10 -0500

    cambios de download

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -5,7 +5,7 @@ import CV from '../../assets/cv.pdf'
 const CTA = () => {
     return (
         <div className='cta'>
-          <a href={CV}Download  className='btn' target="_blank">Descarga CV</a> 
+          <a href={CV}download  className='btn' target="_blank">Descarga CV</a> 
           <a href='#contact' className='btn btn-primary'>Hablemos</a> 
         </div>
     );
