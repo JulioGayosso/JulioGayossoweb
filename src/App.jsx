7044473084a430c3c6a9e032732fa1087e0db9d1
juliogayosso <j_chalo@hotmail.com>
2022-08-23 18:22:40 -0500

    cambios de header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Header from './components/header/Header'
+import Header from './components/Header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
